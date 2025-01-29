7a201303d76af2ba4e47008c2c1616231b644d6b
STK09 <mkeityrs@gmail.com>
2025-01-30 00:47:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/i_killed_my_clan for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Soutick_09 for support")
                 sys.exit()
         if FORCESUB_CHANNEL2:
             try:
@@ -56,7 +56,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL2}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/i_killed_my_clan for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Soutick_09 for support")
                 sys.exit()
         if FORCESUB_CHANNEL3:
             try:
@@ -69,7 +69,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL3}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/i_killed_my_clan for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Soutick_09 for support")
                 sys.exit()
 
         if FORCESUB_CHANNEL4:
@@ -83,7 +83,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL4 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL4}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/i_killed_my_clan for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Soutick_09 for support")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -93,11 +93,11 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/i_killed_my_clan for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Soutick_09 for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/i_killed_my_clan")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Soutick_09")
         self.LOGGER(__name__).info(f""" \n\n       
 ░█████╗░███╗░░██╗██╗███╗░░░███╗███████╗██╗░░██╗██╗░░░██╗███╗░░██╗████████╗███████╗██████╗░░██████╗
 ██╔══██╗████╗░██║██║████╗░████║██╔════╝██║░░██║██║░░░██║████╗░██║╚══██╔══╝██╔════╝██╔══██╗██╔════╝
